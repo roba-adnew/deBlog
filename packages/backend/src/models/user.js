700c1f34121282e1632e3349942404c1533984d6
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-01 14:44:33 -0400

    Created the user model for db

---
+++ packages/backend/src/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true},
+    hashedPassword: { type: String, required: true},
+    start: { 
+        type: Number, 
+        required: true, 
+        default: Date.now() // ms
+    }, 
+    author: { type: Boolean, required: true }
+})
+
+UserSchema.virtual("fullName").get(function() {
+    fullName = `${this.firstName} ${this.lastName}`;
+    return fullName
+})
+
+UserSchema.virtual('url').get(function() {
+    return `/users/${this._id}`
+})
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
