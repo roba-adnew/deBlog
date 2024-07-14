34b9fa5956291135e7450df962513ddcdcaac106
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-14 13:07:09 -0400

    Implemented front-end password validation

--- packages/frontend-reader/src/components/SignUp.jsx
+++ packages/frontend-reader/src/components/SignUp.jsx
@@ -9,6 +9,9 @@ function SignUpForm() {
         username: '',
         password: ''
     })
+    const [passwordsMatch, setPasswordsMatch] = useState(false)
+    const [passwordConfExists, setPasswordConfExists] = useState(false)
+    const [validationMsg, setValidationMsg] = useState('')
 
     function updateUserInfo(e) {
         setUserInfo({
@@ -17,8 +20,27 @@ function SignUpForm() {
         })
     }
 
-    console.log(userInfo)
+    function checkPasswordConf(e) {
+            const passwordConfField = e.target
+            if (passwordConfField.name !== 'passwordConf') return
 
+            if (passwordConfField.value.length === 0) {
+                setPasswordConfExists(false)
+            }
+            else {
+                setPasswordConfExists(true)
+            }
+    
+            if (userInfo.password !== passwordConfField.value) {
+                setPasswordsMatch(false)
+            } 
+            else {
+                setPasswordsMatch(true)
+            }
+    }
+
+
+    console.log(`${passwordsMatch}, ${passwordConfExists}`)
     return (
         <>
             <a href="/">home</a>
@@ -28,33 +50,40 @@ function SignUpForm() {
                     <span>
                         <input
                             text={userInfo.firstName}
+                            name='firstName'
                             placeholder='first name'
                             onChange={updateUserInfo}
                         />
                         <input
                             text={userInfo.lastName}
+                            name='lastName'
                             placeholder='last name'
                             onChange={updateUserInfo}
                         />
                     </span>
                     <input
                         text={userInfo.username}
+                        name='username'
                         placeholder='username'
                         onChange={updateUserInfo}
                     />
                     <input
                         text={userInfo.password}
+                        name='password'
                         placeholder='password'
                         onChange={updateUserInfo}
                     />
                     <input
+                        name='passwordConf'
                         placeholder='re-enter password'
+                        onBlur={checkPasswordConf}
                     />
+                    {passwordConfExists && !passwordsMatch && 
+                    <p id='pwValidator'>passwords dont match</p>}
                     <button>sign-up</button>
                 </form>
             </div>
         </>
-
     )
 }
 
