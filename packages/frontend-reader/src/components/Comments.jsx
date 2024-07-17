fbfc8422e459add84791c30b2553201ae2d16b88
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-16 22:06:17 -0400

    Implemented comment adding form and api call

--- packages/frontend-reader/src/components/Comments.jsx
+++ packages/frontend-reader/src/components/Comments.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
-import { getComments, editComment as apiEditComment } from '../utils/postApi'
+import { getComments, editComment, addComment } from '../utils/postApi'
 
-function Comment({ comment, postId, refetch }) {
+function Comment({ user, comment, postId, refetch }) {
     const [editing, setEditing] = useState(false)
-    const user = JSON.parse(localStorage.getItem('user'))
-    const userIsCommenter = !!user && (user._id === comment.user._id);
+    const loggedIn = !!user
+    const userIsCommenter = loggedIn && (user._id === comment.user._id);
 
     function CommentDisplay() {
         const date = format(new Date(comment.ts), 'MMM-dd')
 
-        function toggleForm() {setEditing(true)}
+        function toggleForm() { setEditing(true) }
 
         return (
             <>
@@ -19,12 +19,12 @@ function Comment({ comment, postId, refetch }) {
                 <p className='content' name='content'>{comment.content}</p>
                 {userIsCommenter &&
                     <div className="editDelete">
-                        <button 
+                        <button
                             onClick={toggleForm}
                             className='editBtn'>
-                                Edit -&nbsp;
+                            Edit -&nbsp;
                         </button>
-                        
+
                         <button className='deleteBtn'> Delete</button>
                     </div>
                 }
@@ -35,14 +35,14 @@ function Comment({ comment, postId, refetch }) {
     function CommentEditForm() {
         const [content, setContent] = useState(comment.content)
 
-        function updateContent(e) {setContent(e.target.value)}
+        function updateContent(e) { setContent(e.target.value) }
 
         async function handleEditSubmission(e) {
             e.preventDefault()
             try {
                 console.log('commencing comment edit')
-                const response = 
-                    await apiEditComment(postId, comment._id, content)
+                const response =
+                    await editComment(postId, comment._id, content)
                 console.log('response', response)
                 console.log('comment updated')
                 refetch(true)
@@ -70,7 +70,7 @@ function Comment({ comment, postId, refetch }) {
         <div className='comment'>
             {editing
                 ? <CommentEditForm />
-                : <CommentDisplay  />
+                : <CommentDisplay />
             }
         </div>
     )
@@ -78,9 +78,12 @@ function Comment({ comment, postId, refetch }) {
 
 function CommentSection({ postId }) {
     const [isLoading, setIsLoading] = useState(true)
-    const [comments, setComments] = useState(null)
+    const [comments, setComments] = useState([])
     const [refetch, setRefetch] = useState(false)
     const [error, setError] = useState(null)
+
+    const user = JSON.parse(localStorage.getItem('user'))
+    const loggedIn = !!user
 
     useEffect(() => {
         async function fetchComments(postId) {
@@ -97,58 +100,81 @@ function CommentSection({ postId }) {
                 console.error(err)
             } finally {
                 setIsLoading(false)
+                setRefetch(false)
             }
         }
         fetchComments(postId)
     }, [refetch])
 
-    if (!comments || comments.length === 0) return
 
     function AddCommentForm() {
+        const [content, setContent] = useState('')
         const [adding, setAdding] = useState(false)
 
-        function addComment(e) {
+        async function handleSubmission(e) {
             e.preventDefault()
-            toggleForm()
+            try {
+                console.log('commencing comment edit')
+                const response =
+                    await addComment(postId, content)
+                console.log('response', response)
+                console.log('comment added')
+                setRefetch(true)
+            } catch (err) {
+                console.error(err)
+                throw err
+            } finally {
+                toggleForm()
+            }
+            
         }
 
-        function toggleForm() {setAdding(!adding)}
+        function updateContent(e) { setContent(e.target.value) }
 
+        function toggleForm() { setAdding(!adding) }
 
         return (
-            adding 
-            ? <form onSubmit={addComment} method='POST'>
-                <input 
-
-                />
-                <button>add comment</button>
-            </form>
-            : <button onClick={toggleForm}>add new comment</button>
+            adding
+                ? <form onSubmit={handleSubmission} method='POST'>
+                    <input
+                        value={content}
+                        onChange={updateContent}
+                    />
+                    <button>add comment</button>
+                    <button onClick={toggleForm}>cancel</button>
+                </form>
+                : <button onClick={toggleForm}>add new comment</button>
         )
     }
 
     return (
-        <div className="commentSection">
-            <p className='commentTitle'>Comments</p>
-            <hr className='commentDivider' />
-            {comments.map((comment, i, array) => {
-                const isLast = i === array.length - 1;
-                return (
-                    <>
-                        <Comment 
-                            key={comment._id} 
-                            postId={postId} 
-                            comment={comment} 
-                            refetch={setRefetch}
-                        />
-                        {!isLast &&
-                            <hr key={comment._id} className='commentDivider' />
-                        }
-                    </>
-                )
-            })}
-            <AddCommentForm />
-        </div>
+        <>
+            {comments.length > 0 &&
+                <div className="commentSection">
+                    <p className='commentTitle'>Comments</p>
+                    <hr className='commentDivider' />
+                    {comments.map((comment, i, array) => {
+                        const isLast = i === array.length - 1;
+                        return (
+                            <>
+                                <Comment
+                                    user={user}
+                                    key={comment._id}
+                                    postId={postId}
+                                    comment={comment}
+                                    refetch={setRefetch}
+                                />
+                                {!isLast &&
+                                    <hr key={comment._id} className='commentDivider' />
+                                }
+                            </>
+                        )
+                    })}
+                </div>
+            }
+            {loggedIn && <AddCommentForm />}
+        </>
+
     )
 }
 
--- packages/frontend-reader/src/utils/postApi.jsx
+++ packages/frontend-reader/src/utils/postApi.jsx
@@ -21,11 +21,9 @@ async function getComments(postId) {
 }
 
 async function editComment(postId, commentId, newContent) {
-    const body = { postId, commentId, newContent}
+    const body = { commentId, newContent}
     const token = JSON.parse(localStorage.getItem('token'))
-    console.log(body)
     try {
-
         const response = await fetch(`http://localhost:4000/api/posts/${postId}/comments/edit`, {
             method: 'PUT',
             headers: { 
@@ -41,4 +39,24 @@ async function editComment(postId, commentId, newContent) {
     }
 }
 
-export { getPosts, getComments, editComment };
+async function addComment(postId, content){
+    const body = { content }
+    const token = JSON.parse(localStorage.getItem('token'))
+    console.log('body', body)
+    try {
+        const response = await fetch(`http://localhost:4000/api/posts/${postId}/comments`, {
+            method: 'POST',
+            headers: { 
+                'Authorization' : `Bearer ${token}`,
+                'Content-type' : 'application/json' 
+            },
+            body: JSON.stringify(body)
+        })
+        const data = await response.json();
+        return data
+    } catch (err) {
+        throw err
+    }
+}
+
+export { getPosts, getComments, editComment , addComment};
