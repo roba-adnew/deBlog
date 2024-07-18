8ddc5b5a4e35e2f796ff06e02adf3a208d7e34d0
roba <10408936+roba-adnew@users.noreply.github.com>
2024-07-18 14:30:11 -0400

    Fixing bug with generating new accessToken

--- packages/frontend-reader/src/utils/authApi.js
+++ packages/frontend-reader/src/utils/authApi.js
@@ -3,12 +3,12 @@ async function signUp(formData) {
     const options = {
         method: 'POST',
         headers: {
-            'Content-type' : 'application/json'
+            'Content-type': 'application/json'
         },
         body: JSON.stringify(formData)
     }
     try {
-        
+
         console.log('new account form data', formData)
         const response = await fetch(url, options)
         console.log('signup response')
@@ -22,7 +22,7 @@ async function login(credentials) {
     const url = 'http://localhost:4000/api/user/login'
     const options = {
         method: 'POST',
-        headers: {'Content-type' : 'application/json'},
+        headers: { 'Content-type': 'application/json' },
         body: JSON.stringify(credentials)
     }
     try {
@@ -51,20 +51,20 @@ async function deleteRefreshToken(userId) {
 
 async function getNewAccessToken() {
     const user = JSON.parse(localStorage.getItem('user'))
-    const url  = 'http://localhost:4000/api/user/refresh-token'
+    const url = 'http://localhost:4000/api/user/refresh-token'
     const options = {
         method: 'POST',
         headers: {
-            'Content-type' : 'application/json'
+            'Content-type': 'application/json'
         },
         body: JSON.stringify({ user })
     }
     try {
         console.log('starting to get new access token')
         const response = await fetch(url, options)
-        const data = response.json()
+        const data = await response.json()
         console.log('refresh results', data)
-        localStorage.setItem('token', data.accessToken)
+        localStorage.setItem('token', JSON.stringify(data))
         return data
     } catch (err) {
         console.error(err)
@@ -72,20 +72,27 @@ async function getNewAccessToken() {
 }
 
 async function fetchWithToken(url, method, body) {
-    let accessToken = JSON.parse(localStorage.getItem('token'))
-    const options = {
-        method, 
-        headers: {
-            'Authorization': `Bearer ${accessToken.token}`,
-            'Content-type':'application/json'
-        },
-        body: JSON.stringify(body)
-    }
-   
+    let accessTokenJSON = localStorage.getItem('token')
+
     try {
-        if (new Date(accessToken.expiresAt).getTime() < Date.now()){
+        if (!accessTokenJSON) await getNewAccessToken();
+        let accessToken = JSON.parse(localStorage.getItem('token'))
+
+        const options = {
+            method,
+            headers: {
+                'Authorization': `Bearer ${accessToken.token}`,
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }
+
+        const tokenExpired = new Date(accessToken.expiresAt).getTime() < Date.now()
+
+        if (tokenExpired) {
             await getNewAccessToken()
             accessToken = JSON.parse(localStorage.getItem('token'))
+            options.headers['Authorization'] = `Bearer ${accessToken.token}`
         }
         const response = await fetch(url, options)
         return response
